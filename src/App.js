d55a2195275a9779584787951e5992907955a173
EnyaDev <enyam.rs@gmail.com>
2023-01-18 17:32:01 -0600

    ahora si

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import NavBar from "./Components/NavBar/NavBar";
 // import ItemCount from "./Components/ItemCount/ItemCount";
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import ItemDetailContainer from "./Components/ItemListContainer/ItemDetail";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
