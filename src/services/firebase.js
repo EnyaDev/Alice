6000a6637969dce87b5db446a2e4ee55abebb3dc
EnyaDev <enyam.rs@gmail.com>
2023-02-19 23:23:12 -0600

    Correccion en Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,7 +7,6 @@ import {
     doc,
     query,
     where,
-    orderBy,
     addDoc,
     writeBatch,
     documentId,
@@ -27,7 +26,7 @@ const db = getFirestore(app);
 
 export async function obtenerProductos() {
     const productsRef = collection(db, "products");
-    const q = query(productsRef, orderBy("index"));
+    const q = query(productsRef);
 const snapshot = await getDocs(q);
 
 const plants = snapshot.docs.map((elem) => {
@@ -72,12 +71,49 @@ const plants = snapshot.docs.map((elem) => {
 return plants;
 }
 
-export async function createOrder_WithStockControl(order) {
+export async function createOrder(order) {
 const orderRef = collection(db, "order");
 
 let respuesta = await addDoc(orderRef, order);
 
 return respuesta.id;
+}
+
+export async function createOrder_WithStockControl(order) {
+  const orderRef = collection(db, "order");
+  const productsRef = collection(db, "products");
+
+  const batch = writeBatch(db);
+
+  const arrayIds = order.items.map((item) => item.id);
+  
+  const q = query(productsRef, where(documentId(), "in", arrayIds));
+  const querySnaphot = await getDocs(q);
+  const docsToUpdate = querySnaphot.docs;
+  let itemsSinStock = [];
+  docsToUpdate.forEach((doc) => {
+
+    let stock = doc.data().stock;
+    let itemInCart = order.items.find((item) => item.id === doc.id);
+    let countInCart = itemInCart.count;
+    let newStock = stock - countInCart;
+
+    if (newStock < 0) {
+      itemsSinStock.push(doc.id);
+    } else {
+      batch.update(doc.ref, { stock: newStock });
+    }
+  });
+
+  if (itemsSinStock.length >= 1)
+    throw new Error(
+      `Stock no disponible para el producto para los productos ${itemsSinStock}`
+    );
+  
+  await batch.commit();
+
+  let newOrder = await addDoc(orderRef, order);
+  return newOrder.id;
 }
 
 export async function exportArray() {
@@ -206,7 +242,6 @@ for (let item of products) {
 }
 
 batch.commit().then(() => console.log("batch listo"));
-
 }
 
 export default db;
